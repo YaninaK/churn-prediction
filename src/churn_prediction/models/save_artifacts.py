d22d43af936155eb6b2f9206889bebfbd6007659
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-09 16:47:14 +0300

    Add train.py and save_artifacts.py

---
+++ src/churn_prediction/models/save_artifacts.py
@@ -0,0 +1,105 @@
+import os
+import logging
+import joblib
+import pandas as pd
+import numpy as np
+from typing import Optional
+
+
+__all__ = ["save_artifacts"]
+
+logger = logging.getLogger()
+
+PATH = ""
+
+FOLDER_2 = "data/02_intermediate/"
+FOLDER_3 = "data/03_primary/"
+
+FOLDER_4 = "data/04_feature/"
+SCALER_LSTM_PATH = FOLDER_5 + "scaler_lstm.joblib"
+SCALER_NUM_PATH = FOLDER_4 + "scaler_num.joblib"
+SELECTED_FEATURES_PATH = FOLDER_4 + "selected_features_nn.joblib"
+
+FOLDER_5 = "data/05_model_input/"
+SEQ_TRAIN_PATH = FOLDER_5 + "seq_train.npy"
+SEQ_VALID_PATH = FOLDER_5 + "seq_valid.npy"
+TRAIN_PATH = FOLDER_5 + "train.parquet.gzip"
+VALID_PATH = FOLDER_5 + "valid.parquet.gzip"
+
+
+def save_lstm_artifacts(
+    lstm_artifacts: list,
+    path: Optional[str] = None,
+    seq_train_path: Optional[str] = None,
+    seq_valid_path: Optional[str] = None,
+    scaler_lstm_path: Optional[str] = None,
+):
+    logging.info("Saving lstm_artifacts...")
+
+    [seq_train, seq_valid, scaler_lstm] = lstm_artifacts
+
+    if path is None:
+        path = PATH
+    if seq_train_path is None:
+        seq_train_path = SEQ_TRAIN_PATH
+    if seq_valid_path is None:
+        seq_valid_path = SEQ_VALID_PATH
+    if scaler_lstm_path is None:
+        scaler_lstm_path = SCALER_LSTM_PATH
+
+    np.save(seq_train_path, seq_train)
+    np.save(seq_valid_path, seq_valid)
+    joblib.dump(scaler_lstm, scaler_lstm_path, 3)
+
+
+def save_nn_model_dataset(
+    nn_model_dataset: list,
+    path: Optional[str] = None,
+    train_path: Optional[str] = None,
+    valid_path: Optional[str] = None,
+):
+    logging.info("Saving nn model dataset...")
+
+    if path is None:
+        path = PATH
+    if train_path is None:
+        train_path = TRAIN_PATH
+    if valid_path is None:
+        valid_path = VALID_PATH
+
+    [X_train, X_valid, y_train, y_valid] = nn_model_dataset
+
+    train = pd.concat([X_train, y_train], axis=1)
+    valid = pd.concat([X_valid, y_valid], axis=1)
+    train.to_parquet(train_path, compression="gzip")
+    valid.to_parquet(valid_path, compression="gzip")
+
+
+def save_scaler_num(
+    scaler_num,
+    path: Optional[str] = None,
+    scaler_num_path: Optional[str] = None,
+):
+    logging.info("Saving scaler_num...")
+
+    if path is None:
+        path = PATH
+    if scaler_num_path is None:
+        scaler_num_path = SCALER_NUM_PATH
+
+    joblib.dump(scaler_num, scaler_num_path, 3)
+
+
+def save_selected_features(
+    selected_features: list,
+    path: Optional[str] = None,
+    selected_features_path: Optional[str] = None,
+):
+    logging.info("Saving selected_features...")
+
+    if path is None:
+        path = PATH
+    if selected_features_path is None:
+        selected_features_path = SELECTED_FEATURES_PATH
+
+    joblib.dump(selected_features, selected_features_path, 3)
---
+++ src/churn_prediction/models/train.py
@@ -0,0 +1,104 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "src", "churn_prediction"))
+
+import logging
+import numpy as np
+import pandas as pd
+from pickle import dump
+from typing import Optional
+
+from sklearn.preprocessing import StandardScaler
+
+from data.make_dataset import generate_dataset
+from data.validation import train_test_split
+from features.time_series_features import fit_transform_seq, transform_seq
+from features.binary_features import map_categorical_features
+from .save_artifacts import (
+    save_lstm_artifacts,
+    save_nn_model_dataset,
+    save_scaler_num,
+    save_selected_features,
+)
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["preprocess_data"]
+
+
+PATH = ""
+CATEGORICAL_VARIABLES = [
+    "customersuspended",
+    "gender",
+    "homeowner",
+    "maritalstatus",
+    "usesinternetservice",
+    "usesvoiceservice",
+]
+SELECTED_NUMERIC_FEATURES = [
+    "callfailurerate",
+    "numberofcomplaints",
+    "age",
+    "unpaidbalance",
+]
+
+
+def data_preprocessing_pipeline(
+    data: pd.DataFrame,
+    categorical_variables: Optional[list] = None,
+    selected_numeric_features: Optional[list] = None,
+    save_artifacts=True,
+):
+    if categorical_variables is None:
+        categorical_variables = CATEGORICAL_VARIABLES
+    if selected_numeric_features is None:
+        selected_numeric_features = SELECTED_NUMERIC_FEATURES
+
+    logging.info(
+        "Generating dataset from raw data and mapping binary categorical features..."
+    )
+
+    df = generate_dataset(data)
+    df = map_categorical_features(df)
+
+    logging.info("Splitting dataset into train and validation parts...")
+
+    X_train, X_valid, y_train, y_valid = train_test_split(df)
+
+    logging.info("Generating input for LSTM model...")
+
+    scaler_lstm, seq_train = fit_transform_seq(X_train)
+    seq_valid = transform_seq(X_valid, scaler_lstm)
+    lstm_artifacts = [seq_train, seq_valid, scaler_lstm]
+
+    logging.info("Normalizing selected numeric features...")
+
+    scaler_num = StandardScaler()
+    X_train[selected_numeric_features] = scaler_num.fit_transform(
+        X_train[selected_numeric_features]
+    )
+    X_valid[selected_numeric_features] = scaler_num.transform(
+        X_valid[selected_numeric_features]
+    )
+    nn_model_dataset = [X_train, X_valid, y_train, y_valid]
+
+    logging.info("Finalizing the list of selected features...")
+
+    selected_features = selected_numeric_features + categorical_variables
+
+    if save_artifacts:
+        logging.info("Saving artifacts...")
+
+        save_lstm_artifacts(lstm_artifacts)
+        save_nn_model_dataset(nn_model_dataset)
+        save_scaler_num(scaler_num)
+        save_selected_features(selected_features)
+
+    return (
+        lstm_artifacts,
+        nn_model_dataset,
+        scaler_num,
+        selected_features,
+    )
