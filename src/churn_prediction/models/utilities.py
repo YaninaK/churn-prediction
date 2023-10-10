cf2d6def0935b5824f7138f1e4ee822597e661f6
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-10 12:21:52 +0300

    Update utilities.py

--- src/churn_prediction/models/utilities.py
+++ src/churn_prediction/models/utilities.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -51,3 +53,42 @@ def plot_metrics(history):
             plt.ylim([0, 1])
 
         plt.legend()
+
+
+def plot_cm(labels, predictions, threshold=0.5):
+    cm = confusion_matrix(labels, predictions > threshold)
+    plt.figure(figsize=(5, 5))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title("Confusion matrix @{:.2f}".format(threshold))
+    plt.ylabel("Actual label")
+    plt.xlabel("Predicted label")
+
+    print("No churn Detected (True Negatives): ", cm[0][0])
+    print("No churn Incorrectly Detected (False Positives): ", cm[0][1])
+    print("Churn Missed (False Negatives): ", cm[1][0])
+    print("Churn Detected (True Positives): ", cm[1][1])
+    print("Total Fraudulent Transactions: ", np.sum(cm[1]))
+
+
+def plot_roc(name, labels, predictions, **kwargs):
+    fp, tp, _ = roc_curve(labels, predictions)
+
+    plt.plot(100 * fp, 100 * tp, label=name, linewidth=2, **kwargs)
+    plt.xlabel("False positives [%]")
+    plt.ylabel("True positives [%]")
+    plt.title("ROC Curve")
+    plt.grid(True)
+    ax = plt.gca()
+    ax.set_aspect("equal")
+
+
+def plot_prc(name, labels, predictions, **kwargs):
+    precision, recall, _ = precision_recall_curve(labels, predictions)
+
+    plt.plot(precision, recall, label=name, linewidth=2, **kwargs)
+    plt.xlabel("Precision")
+    plt.ylabel("Recall")
+    plt.title("Precision-Recall curve")
+    plt.grid(True)
+    ax = plt.gca()
+    ax.set_aspect("equal")
