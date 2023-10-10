308f0d66cf4ceb6e669627dc3b405384d2972f6f
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-10 23:34:06 +0300

    Update binary_features.py

--- src/churn_prediction/features/binary_features.py
+++ src/churn_prediction/features/binary_features.py
@@ -2,7 +2,15 @@ import logging
 import pandas as pd
 
 
+logger = logging.getLogger(__name__)
+
+__all__ = ["map_categorical_features"]
+
+
 def map_categorical_features(df: pd.DataFrame) -> pd.DataFrame:
+
+    logging.info("Mapping categorical features features...")
+
     X = df.copy()
     X["customersuspended"] = X["customersuspended"].map({"Yes": 1, "No": 0})
     X["gender"] = X["gender"].map({"Male": 1, "Female": 0})
