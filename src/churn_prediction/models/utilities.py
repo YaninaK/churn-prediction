fa77648915077d2948219972b0e0b6f3cbdd7a6f
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-10 11:11:13 +0300

    All plot_metrics to utilities.py

--- src/churn_prediction/models/utilities.py
+++ src/churn_prediction/models/utilities.py
@@ -29,3 +29,25 @@ def plot_loss(history):
     plt.ylabel("Loss")
     plt.title("LSTM model Loss")
     plt.legend()
+
+
+def plot_metrics(history):
+    metrics = ["loss", "prc", "precision", "recall"]
+    plt.figure(figsize=(10, 8))
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_", " ").capitalize()
+        plt.subplot(2, 2, n + 1)
+        plt.plot(history.epoch, history.history[metric], label="Train")
+        plt.plot(
+            history.epoch, history.history["val_" + metric], linestyle="--", label="Val"
+        )
+        plt.xlabel("Epoch")
+        plt.ylabel(name)
+        if metric == "loss":
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == "auc":
+            plt.ylim([0.8, 1])
+        else:
+            plt.ylim([0, 1])
+
+        plt.legend()
