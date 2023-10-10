2dd3e325b0d1b27e0373ae0cdca46cb38892e7bf
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-11 01:23:56 +0300

    Update inference_tools.py

--- src/churn_prediction/models/inference_tools.py
+++ src/churn_prediction/models/inference_tools.py
@@ -15,9 +15,10 @@ from features.binary_features import map_categorical_features
 from features.time_series_features import transform_seq
 
 
-__all__ = ["save_artifacts"]
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger()
+__all__ = ["inference_preprocessing_pipeline"]
+
 
 PATH = ""
 
@@ -55,14 +56,23 @@ def preprocessing_pipeline(
     X_test = df.drop(TARGET_NAME, axis=1)
     test_labels = df[[TARGET_NAME]]
 
+    logging.info("Loading scaler_lstm...")
+
     scaler_lstm = joblib.load(scaler_lstm_path)
     seq_test = transform_seq(X_test, scaler_lstm)
 
+    logging.info("Loading selected_numeric_features...")
+
     selected_numeric_features = joblib.load(selected_numeric_features_path)
+
+    logging.info("Loading scaler_num...")
+
     scaler_num = joblib.load(scaler_num_path)
     X_test[selected_numeric_features] = scaler_num.transform(
         X_test[selected_numeric_features]
     )
+    logging.info("Loading selected_features...")
+
     selected_features = joblib.load(selected_features_path)
 
     test_features = [
