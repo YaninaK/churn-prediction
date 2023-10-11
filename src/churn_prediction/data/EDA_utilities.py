7ec74c6fb1782dc9b01bacb07403bdb350b2498b
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-10-11 13:58:55 +0300

    Add src\churn_prediction\data\EDA_utilities.py

---
+++ src/churn_prediction/data/EDA_utilities.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def get_correlated_features(df, numeric_variables, threshold=0.95):
+    corr = df[numeric_variables].corr()
+    for i in range(len(numeric_variables)):
+        for j in range(i + 1, len(numeric_variables)):
+            if abs(corr.iloc[i, j]) > threshold:
+                corr_ = round(corr.iloc[i, j], 2)
+                print(corr_, numeric_variables[i], numeric_variables[j])
+
+
+def plot_ks_test(df, feature, control=""):
+    df_ks = pd.DataFrame()
+    df_ks[feature] = np.sort(df[feature].unique())
+    df_ks["F_no_churn"] = df_ks[feature].apply(lambda x: np.mean(no_churn <= x))
+    df_ks["F_churn"] = df_ks[feature].apply(lambda x: np.mean(churn <= x))
+
+    k = np.argmax(np.abs(df_ks["F_no_churn"] - df_ks["F_churn"]))
+    ks_stat = np.abs(df_ks["F_churn"][k] - df_ks["F_no_churn"][k])
+    y = (df_ks["F_churn"][k] + df_ks["F_no_churn"][k]) / 2
+
+    plt.figure(figsize=(5, 4))
+    plt.plot(feature, "F_no_churn", data=df_ks, label="No_churn")
+    plt.plot(feature, "F_churn", data=df_ks, label="Churn")
+    plt.errorbar(
+        x=df_ks[feature][k],
+        y=y,
+        yerr=ks_stat / 2,
+        color="k",
+        capsize=5,
+        mew=3,
+        label=f"Test statistic: {ks_stat:.4f}",
+    )
+    plt.legend(loc="center right")
+    plt.title(f"Kolmogorov-Smirnov Test\n{feature}")
